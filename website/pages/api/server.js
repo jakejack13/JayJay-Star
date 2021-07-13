5bdc49136b74a2c78a3140f44e4dc91bf705d2c7
warren yun <xxwarrenhdxx@gmail.com>
2021-07-12 22:11:49 -0400

    Shorten handlers to one function

--- website/pages/api/server.js
+++ website/pages/api/server.js
@@ -1,16 +1,9 @@
-// Saving latest starbaord data to json file
-export default function updateData(req, res) {
-  if (req.method == 'POST') {
-    res.status(200).json({ message: 'Updated data!' })
-  } else {
-    res.status(400).json({ message: 'Bad request error' })
-  }
-}
-
-// Get latest starboard data to load into website
-export default function getData(req, res) {
+// Handler for starboard data
+export default function dataHandler(req, res) {
   if (req.method == 'GET') {
     res.status(200).json({ message: 'data' })
+  } else if (req.method == 'POST') {
+    res.status(200).json({ message: 'Updated data!' })
   } else {
     res.status(400).json({ message: 'Bad request error' })
   }
