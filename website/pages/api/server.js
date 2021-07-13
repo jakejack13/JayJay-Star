df796a87f01b6f69418f96869189e4105da35c68
warren yun <xxwarrenhdxx@gmail.com>
2021-07-12 23:21:25 -0400

    Use switch statements for backend server

--- website/pages/api/server.js
+++ website/pages/api/server.js
@@ -1,10 +1,15 @@
 // Handler for starboard data
 export default function dataHandler(req, res) {
-  if (req.method == 'GET') {
-    res.status(200).json({ message: 'data' })
-  } else if (req.method == 'POST') {
-    res.status(200).json({ message: 'Updated data!' })
-  } else {
-    res.status(400).json({ message: 'Bad request error' })
+  switch(req.method) {
+    case 'GET':
+      res.status(200).json({ message: 'data' });
+      break;
+    case 'POST':
+      res.status(200).json({ message: 'Updated data!' });
+      console.log(req.body.query)
+      break;
+    default: 
+      res.status(400).json({ message: 'Bad request error' });
+      break;
   }
 }
