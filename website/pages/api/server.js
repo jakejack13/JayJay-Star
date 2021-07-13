c1f00ff935d07cd9ad57961563e94ed5db4ab3f9
warren yun <xxwarrenhdxx@gmail.com>
2021-07-13 17:07:31 -0400

    Add temporary storing system

--- website/pages/api/server.js
+++ website/pages/api/server.js
@@ -1,13 +1,23 @@
+const fs = require('fs');
+
 // Handler for starboard data
 export default function dataHandler(req, res) {
   switch(req.method) {
     case 'GET':
       res.status(200).json({ message: 'data' });
       break;
+
     case 'POST':
       res.status(200).json({ message: 'Updated data!' });
-      console.log(req.body.query)
+      const newData = JSON.stringify(req.body);
+      fs.writeFileSync("data.json", newData, 'utf-8', function (err) {
+        if (err) {
+          console.log("error writing to json file");
+          return console.log(err);
+        }
+      });
       break;
+      
     default: 
       res.status(400).json({ message: 'Bad request error' });
       break;
