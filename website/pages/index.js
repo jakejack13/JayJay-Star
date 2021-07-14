12698aaf879388545efcd41d17ba21ee8cc65b16
warren yun <xxwarrenhdxx@gmail.com>
2021-07-14 19:48:49 -0400

    Add get static props

--- website/pages/index.js
+++ website/pages/index.js
@@ -1,4 +1,6 @@
 import homeStyles from '../styles/Home.module.css';
+import axios from 'axios';
+
 
 export default function Home(props) {
   return (
@@ -9,5 +11,15 @@ export default function Home(props) {
 }
 
 export const getStaticProps = async () => {
-  
+  var posts;
+  await (async () => {
+    const allPosts = await axios.get("http://localhost:3000/api/server");
+    posts = allPosts.data;
+  })();
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
