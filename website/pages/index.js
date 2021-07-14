4a27f92dab0c118c77d3ac9da2a04b24c0e68efb
warren yun <xxwarrenhdxx@gmail.com>
2021-07-14 16:41:26 -0400

    Install axios

--- website/pages/index.js
+++ website/pages/index.js
@@ -1,9 +1,13 @@
 import homeStyles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div className={homeStyles.container}>
       <h1 className={homeStyles.title}>JayJay Star</h1>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  
 }
